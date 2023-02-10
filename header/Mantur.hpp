f36da9e24eec82000a02fcbd48033b52347f1575
Yaroslav <kogutyaroslav3947@gmail.com>
2023-02-10 13:48:11 +0200

    bugFix Mantur.hpp

--- header/Mantur.hpp
+++ header/Mantur.hpp
@@ -5,5 +5,5 @@ void inputPatients();
 void printPatient(const Patient &patient);
 void addPatientToBinaryFile(Patient patient);
 void savePatientsToBinaryFile(Patient *patients, int size);
-void readPatientsFromBinaryFile(Patient *&patients, int &size);
+void readPatientsFromBinaryFile();
 void saveResultToTextFile(const Patient *patients, int size, ReportType reportType);
